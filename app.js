7cd6f752f8a774807e4cfe19655a9a3e59eb757b
Amin Shazrin <aminshazrin@yahoo.com>
2018-04-05 01:20:41 +0800

    Wow , now i know how to output all those pretty object printing

--- app.js
+++ app.js
@@ -5,5 +5,6 @@ request({
     url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA',
     json: true // this is gonna be request to confirm that it is a json data
 } , (error , response , body) => {
-    console.log(body);
+    // JSON.stringify(body , no value here and just put undefined , this is where you can put number of space you want)
+    console.log(JSON.stringify(body , undefined , 2));
 });
